9f05fe92bec8203cd8324e831b700e0873cecc70
rosenfence <rosenfence@gmail.com>
2022-12-27 20:44:45 +0900

    use websocket to make chatting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,40 @@
-alert('hello');
+const messageList = document.querySelector('ul');
+const nicknameForm = document.querySelector('#nickname');
+const messageForm = document.querySelector('#message');
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+socket.addEventListener('open', () => {
+  console.log('Connected to Server ✅');
+});
+
+socket.addEventListener('message', (message) => {
+  const li = document.createElement('li');
+  li.innerText = message.data.toString();
+  messageList.append(li);
+});
+
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server ❌');
+});
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+};
+
+const handleNicknameSubmit = (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  input.value = '';
+};
+
+messageForm.addEventListener('submit', handleSubmit);
+nicknameForm.addEventListener('submit', handleNicknameSubmit);
