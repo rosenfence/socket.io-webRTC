d9b499a70071cd5089830d5f843e76cf8bd1e489
rosenfence <rosenfence@gmail.com>
2023-01-01 01:03:35 +0900

    version 3. use WebRTC to make chat start

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -28,8 +28,12 @@ const handleMessageSubmit = (event) => {
 const handleNicknameSubmit = (event) => {
   event.preventDefault();
   const input = room.querySelector('#name input');
+  const button = room.querySelector('#name button');
+  const h4 = room.querySelector('h4');
   const value = input.value;
-  socket.emit('nickname', input.value);
+  socket.emit('nickname', value);
+  button.innerText = 'Change';
+  h4.innerText = `Your current nickname: ${value}`;
 };
 
 const showRoom = () => {
@@ -76,3 +80,87 @@ socket.on('room_change', (rooms) => {
     roomList.append(li);
   });
 });
+
+//----------------------------------------------------------------
+
+const myFace = document.getElementById('myFace');
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
+const camerasSelect = document.getElementById('cameras');
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === 'videoinput');
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement('option');
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+const getMedia = async (deviceId) => {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: 'user' },
+  };
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(deviceId ? cameraConstrains : initialConstrains);
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+getMedia();
+
+const handleMuteClick = () => {
+  myStream.getAudioTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
+  if (!muted) {
+    muteBtn.innerText = 'Unmute';
+    muted = true;
+  } else {
+    muteBtn.innerText = 'Mute';
+    muted = false;
+  }
+};
+const handleCameraClick = () => {
+  myStream.getVideoTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
+  if (cameraOff) {
+    cameraBtn.innerText = 'Turn Camera Off';
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = 'Turn Camera On';
+    cameraOff = true;
+  }
+};
+
+const handleCameraChange = async () => {
+  await getMedia(camerasSelect.value);
+};
+
+muteBtn.addEventListener('click', handleMuteClick);
+cameraBtn.addEventListener('click', handleCameraClick);
+camerasSelect.addEventListener('click', handleCameraChange);
